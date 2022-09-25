afa8bcfe452d355279ea6738af838cb87f7392fa
Sbastdia <91331243+Sbastdia@users.noreply.github.com>
2022-09-25 16:39:16 +0000

    más cambios

--- gestor/run.py
+++ gestor/run.py
@@ -3,10 +3,10 @@ import menu
 import ui
 
 if __name__ == "__main__":
-   # Si pasamos un argumento -t lanzamos el modo terminal
-   if len(sys.argv) > 1 and sys.argv[1] == "-t":
-     menu.iniciar()
-   # En cualquier otro caso lanzamos el modo gráfico
-   else:
-     app = ui.MainWindow()
-     app.mainloop()
+    # Si pasamos un argumento -t lanzamos el modo terminal
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.iniciar()
+    # En cualquier otro caso lanzamos el modo gráfico
+    else:
+        app = ui.MainWindow()
+        app.mainloop()
